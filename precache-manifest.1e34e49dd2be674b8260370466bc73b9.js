8b66cea1ac76b9ae9beb2da010ecd2358bb00a46
loc-koan <loc.koan@yahoo.com>
2020-09-25 22:21:51 -0500

    Updates

---
+++ precache-manifest.1e34e49dd2be674b8260370466bc73b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18436815e883506a8b38a9b026508d8b",
+    "url": "/go-fish/index.html"
+  },
+  {
+    "revision": "79df607e15e3ba89c85f",
+    "url": "/go-fish/static/css/main.f14eac9b.chunk.css"
+  },
+  {
+    "revision": "332935dbfdc77a1b1755",
+    "url": "/go-fish/static/js/2.021d4f26.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/go-fish/static/js/2.021d4f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79df607e15e3ba89c85f",
+    "url": "/go-fish/static/js/main.6b54d9a1.chunk.js"
+  },
+  {
+    "revision": "60f65a566d8d80226a78",
+    "url": "/go-fish/static/js/runtime-main.6c6f1101.js"
+  }
+]);
